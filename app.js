9a88b23c09d3054836e3adc49247cec18cda495c
Randy Lu <djyde520@gmail.com>
2012-07-24 14:28:13 +0800

    fix

--- app.js
+++ app.js
@@ -2,15 +2,12 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , routes = require('./routes')
   , http = require('http');
-
 var app = express();
-
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 80);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
@@ -21,11 +18,9 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
-
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
 app.get('/', routes.index);
 app.post('/push',routes.pushExt);
 app.get('/type/:type',routes.getExt);
@@ -34,7 +29,6 @@ app.get('/admin',routes.checkLogin,routes.admin);
 app.get('/login',routes.login);
 app.post('/login',routes.doLogin);
 app.post('/pass',routes.pass);
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
